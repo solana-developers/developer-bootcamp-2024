300a701cc01012ee24396865e391d3ccb6f2f071
brimigs <85972460+brimigs@users.noreply.github.com>
2025-04-21 16:44:10 -0400

    update token vesting to anchor 31.1

--- project-8-token-vesting/anchor/programs/vesting/src/lib.rs
+++ project-8-token-vesting/anchor/programs/vesting/src/lib.rs
@@ -207,3 +207,37 @@ pub enum ErrorCode {
     #[msg("There is nothing to claim.")]
     NothingToClaim,
 }
+
+#[(test)]
+mod test {
+    use super::*;
+    use litesvm::LiteSVM;
+    use solana_program::{message::Message, pubkey::Pubkey, system_instruction::transfer};
+
+    #[test]
+    fn testing() {
+        let from_keypair = Keypair::new();
+        let from = from_keypair.pubkey();
+        let to = Pubkey::new_unique();
+
+        let mut svm = LiteSVM::new();
+        svm.airdrop(&from, 10_000).unwrap();
+
+        let instruction = transfer(&from, &to, 64);
+        let tx = Transaction::new(
+            &[&from_keypair],
+            Message::new(&[instruction], Some(&from)),
+            svm.latest_blockhash(),
+        );
+        let tx_res = svm.send_transaction(tx).unwrap();
+        println!("{:?}", tx_res);
+
+        let from_account = svm.get_account(&from);
+        let to_account = svm.get_account(&to);
+        assert_eq!(from_account.unwrap().lamports, 4936);
+        assert_eq!(to_account.unwrap().lamports, 64);
+    }
+
+    #[test]
+    fn initialize() {}
+}
