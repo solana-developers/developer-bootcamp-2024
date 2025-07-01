032f4e61b17c9275733d4d64b770f01df496a2ab
Lewis <baiwfg@gmail.com>
2025-07-01 17:24:52 +0800

    enhance test code and provide more explanation in project 2(#49)

--- project-2-voting/anchor/tests/basic.spec.ts
+++ project-2-voting/anchor/tests/basic.spec.ts
@@ -3,11 +3,16 @@ import { Program } from '@coral-xyz/anchor';
 import { Voting } from '../target/types/voting';
 import { PublicKey } from '@solana/web3.js';
 
+// This is "non-Bankrun" way of writing cases.In this case you must use `solana-test-validator`
+// automatically(`anchor test` would do that) or
+// manually(run `solana-test-validator` in separate terminal)
+
 describe('Voting', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Voting as Program<Voting>;
+  console.log("program id: ", program.programId);
 
   it('initializePoll', async () => {
 
@@ -15,17 +20,21 @@ describe('Voting', () => {
       [Buffer.from("poll"), new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
       program.programId
     );
+    // better to wrap program's instruction calling in case that program doesn't
+    // exists in chain and the call just abandons the error
+    try {
+      const tx = await program.methods.initializePoll(
+          new anchor.BN(1),
+          new anchor.BN(0),
+          new anchor.BN(1759508293),
+          "test-poll",
+          "description",
+      ).rpc();
 
-    const tx = await program.methods.initializePoll(
-        new anchor.BN(1),
-        new anchor.BN(0),
-        new anchor.BN(1759508293),
-        "test-poll",
-        "description",
-    )
-    .rpc();
-
-    console.log('Your transaction signature', tx);
+      console.log('Your transaction signature', tx);
+    } catch (e) {
+      console.error('initializePoll failed:', e);
+    }
   });
 
   it('initialize candidates', async () => {
